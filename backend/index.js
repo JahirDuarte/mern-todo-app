7d20395ad91a838f803504e4cf88988195337ac5
jahirduarte <jahir.duarte.inf@gmail.com>
2024-01-16 13:35:28 -0600

    Methods GET, POSt, PUT, DELETE for Todo

--- backend/index.js
+++ backend/index.js
@@ -32,11 +32,32 @@ app.on('close', () => {
 });
 
 // Endpoints
+app.get('/get', (req, res) => {
+    const task = req.body.task;
+    TodoModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
 app.post('/add', (req, res) => {
     const task = req.body.task;
     TodoModel.create({
         task:task
     })
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.put('/update/:id', (req, res) => {
+    const {id} = req.params;
+    TodoModel.findByIdAndUpdate({_id: id}, {done: true})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.delete('/delete/:id', (req, res) => {
+    const {id} = req.params;
+    TodoModel.findByIdAndDelete({_id: id})
     .then(result => res.json(result))
     .catch(err => res.json(err))
 })
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,10 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import Create from './Create'
 import axios from 'axios'
+import { BsCircleFill, BsFillCheckCircleFill, BsFillTrashFill } from 'react-icons/bs';
 
 function Home() {
     const [todos, setTodos] = useState([])
     
+    useEffect(() => {
+        axios.get('http://localhost:3001/get')
+        .then(result => setTodos(result.data))
+        .catch(err => console.log(err))
+    }, [])
+
+    const handleEdit = (id) => {
+        axios.put('http://localhost:3001/update/'+id)
+        .then(result => {
+            location.reload()
+        })
+        .catch(err => console.log(err))
+    }
+    const handleDelete = (id) => {
+        axios.delete('http://localhost:3001/delete/'+id)
+        .then(result => {
+            location.reload()
+        })
+        .catch(err => console.log(err))
+    }
+
     return (
         <div className='home'>
             <h2>Todo List</h2>
@@ -15,8 +37,22 @@ function Home() {
                 <div><h5>Aún no hay tareas</h5></div>
                 :
                 todos.map(todo => (
-                    <div>
-                        {todo}
+                    <div className="task">
+                        <div className="checkbox" onClick={() => handleEdit(todo._id)}>
+                            {todo.done ?
+                            <BsFillCheckCircleFill className="icon" />
+                            :
+                            <BsCircleFill className="icon" />
+                            }
+                            
+                            <p className={todo.done ? "line_through" : ""}>{todo.task}</p>
+                        </div>
+                        <div>
+                            <span><BsFillTrashFill 
+                                className="icon"
+                                onClick={() => handleDelete(todo._id)}
+                            /></span>
+                        </div>
                     </div>
                 ))
             }
