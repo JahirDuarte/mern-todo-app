c821cba008a4dbc05bb18cbfae1df1403d09abc4
jahirduarte <jahir.duarte.inf@gmail.com>
2024-01-16 10:44:55 -0600

    Creation of Home and Create components

---
+++ frontend/src/Create.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Create() {
+    return (
+        <div className='create_form'>
+            <input type="text" name="" id="" />
+            <button type="button">Agregar tarea</button>
+        </div>
+    )
+}
+
+export default Create
